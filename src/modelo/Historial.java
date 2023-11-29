58a8bbb3828d7f0f7d4b83610d95ea2641ac17c7
Briancardenas12 <brianalexis50@hotmail.com>
2023-11-29 13:17:59 -0500

    first commit - Implementacion de la interfaz gráfica del aplicativo de registro de la veterinaria

---
+++ src/modelo/Historial.java
@@ -0,0 +1,44 @@
+
+package modelo;
+
+public class Historial {
+    
+    private int codigo;
+    private String descripcion;
+    private Procedimiento procedimientos;
+
+    public Historial() {
+        procedimientos = new Procedimiento();
+    }
+
+    public Historial(int codigo, String descripcion, Procedimiento procedimientos) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.procedimientos = procedimientos;
+    }
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Procedimiento getProcedimientos() {
+        return procedimientos;
+    }
+
+    public void setProcedimientos(Procedimiento procedimientos) {
+        this.procedimientos = procedimientos;
+    }
+}
